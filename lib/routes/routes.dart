b2a248a49ab457c7e0ba4c01c125bb02925e303d
Maxi <maidana1454@gmail.com>
2024-02-19 23:27:01 -0300

    se mueven las rutas

--- lib/routes/routes.dart
+++
@@ -1,7 +0,0 @@
-class Routes {
-  static const String main = '/';
-  static const String auth = '/auth';
-  static const String home = '/home';
-  static const String addClient = '/addClient';
-  static const String client = '/client';
-}
