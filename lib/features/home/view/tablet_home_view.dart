802c031cbed527ff6d6513f65dc21d7f01a92a92
Maxi <maidana1454@gmail.com>
2024-02-20 09:01:31 -0300

    se empieza a dibujar el home

--- lib/features/home/view/tablet_home_view.dart
+++ lib/features/home/view/tablet_home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
 
 class TabletHomeView extends StatefulWidget {
   const TabletHomeView({super.key});
@@ -8,12 +9,55 @@ class TabletHomeView extends StatefulWidget {
 }
 
 class _TabletHomeViewState extends State<TabletHomeView> {
-  final TextEditingController userController = TextEditingController();
-  final TextEditingController passwordController = TextEditingController();
+  double widthMenu = 20.w;
+  bool isOpen = true;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('sisi home tablet'),
+    return Row(
+      children: [
+        Padding(
+          padding: EdgeInsets.only(right: 0.5.w),
+          child: AnimatedContainer(
+            duration: const Duration(milliseconds: 300),
+            width: isOpen ? 20.w : 5.w,
+            decoration: BoxDecoration(
+                color: Colors.grey[50],
+                borderRadius: const BorderRadius.only(
+                    bottomRight: Radius.circular(12),
+                    topRight: Radius.circular(12)),
+                boxShadow: const [
+                  BoxShadow(
+                      offset: Offset(0.5, 0),
+                      color: Colors.grey,
+                      blurRadius: 4,
+                      spreadRadius: 0.5,
+                      blurStyle: BlurStyle.normal)
+                ]),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                IconButton(
+                  icon: const Icon(Icons.menu),
+                  onPressed: () {
+                    if (isOpen) {
+                      widthMenu = 5.w;
+                    } else {
+                      widthMenu = 20.w;
+                    }
+                    isOpen = !isOpen;
+                    setState(() {});
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+        AnimatedContainer(
+          duration: const Duration(milliseconds: 300),
+          width: isOpen ? 79.5.w : 94.5.w,
+          color: Colors.grey[100],
+        ),
+      ],
     );
   }
 }
