a9fae5f6c3262f21ccc463f9cd2c90315fbed805
Maxi <maidana1454@gmail.com>
2024-02-19 21:01:48 -0300

    add responsive

---
+++ lib/core/widget_tree.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:gestor_de_clientes/core/responsive_layout.dart';
+
+class WidgetTree extends StatefulWidget {
+  final Widget tiny;
+  final Widget phone;
+  final Widget tablet;
+  final Widget largeTablet;
+  final Widget computer;
+  const WidgetTree({
+    Key? key,
+    required this.tiny,
+    required this.phone,
+    required this.tablet,
+    required this.largeTablet,
+    required this.computer,
+  }) : super(key: key);
+
+  @override
+  State<WidgetTree> createState() => _WidgetTreeState();
+}
+
+class _WidgetTreeState extends State<WidgetTree> {
+  @override
+  Widget build(BuildContext context) {
+    return ResponsiveLayout(
+      tiny: widget.tiny,
+      phone: widget.phone,
+      tablet: widget.tablet,
+      largeTablet: widget.largeTablet,
+      computer: widget.computer,
+    );
+  }
+}
